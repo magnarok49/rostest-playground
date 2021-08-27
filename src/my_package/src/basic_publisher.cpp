5bdbb2835b64f6e668bfd0c2da5e5b2dc6f3272e
Magne Sirnes <magne.sirnes@zeabuz.com>
2021-08-27 13:34:36 +0200

    added more tests, and set up auto-formatting

--- src/my_package/src/basic_publisher.cpp
+++ src/my_package/src/basic_publisher.cpp
@@ -1,28 +1,31 @@
+#include <sstream>
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include <sstream>
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle myNode;
+  ros::Publisher chatter_pub = myNode.advertise<std_msgs::String>("helloWorldSpam", 100);
+  ros::Rate loop_rate(10);
 
-int main(int argc, char** argv){
-    ros::init(argc, argv, "talker");
-    ros::NodeHandle myNode;
-    ros::Publisher chatter_pub = myNode.advertise<std_msgs::String>("helloWorldSpam", 100);
-    ros::Rate loop_rate(10);
+  int count = 0;
 
-    int count = 0;
+  while (ros::ok())
+  {
+    std_msgs::String myMsg;
+    std::stringstream ss;
+    ss << "hello world " << count;
+    myMsg.data = ss.str();
 
-    while(ros::ok()){
-        std_msgs::String myMsg;
-        std::stringstream ss;
-        ss << "hello world " << count;
-        myMsg.data = ss.str();
+    // ROS_INFO("%s", myMsg.data.c_str());
 
-        ROS_INFO("%s", myMsg.data.c_str());
+    chatter_pub.publish(myMsg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
 
-        chatter_pub.publish(myMsg);ros::spinOnce();
-        loop_rate.sleep();
-        ++count;
-    }
-
-    return 0;
+  return 0;
 }
--- src/my_package/test/basic_testsuite.cpp
+++ src/my_package/test/basic_testsuite.cpp
@@ -1,37 +1,59 @@
-#include "ros/ros.h"
-#include <ros/console.h>
-#include "std_msgs/String.h"
-
 #include <gtest/gtest.h>
+#include <ros/console.h>
 
 #include <sstream>
 
-class MyTestFixture : public ::testing::Test {
- protected:
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+static int myGlobalCounter = 0;
+
+class MyTestFixture : public ::testing::Test
+{
+protected:
   // You can remove any or all of the following functions if their bodies would
   // be empty.
   std::string desired_topic;
   std::string desired_message_type;
+  ros::NodeHandle n;
+  int messagesReceived;
 
-  MyTestFixture() {
-     // You can do set-up work for each test here.
+  MyTestFixture()
+  {
+    // You can do set-up work for each test here.
   }
 
-  ~MyTestFixture() override {
-     // You can do clean-up work that doesn't throw exceptions here.
+  ~MyTestFixture() override
+  {
+    // You can do clean-up work that doesn't throw exceptions here.
   }
 
   // If the constructor and destructor are not enough for setting up
   // and cleaning up each test, you can define the following methods:
 
-  void SetUp() override {
-     desired_topic = "/helloWorldSpam";
-     desired_message_type = "std_msgs/String";
+  void SetUp() override
+  {
+    // TODO these should be read from a paramserver
+    desired_topic = "/helloWorldSpam";
+    desired_message_type = "std_msgs/String";
   }
 
-  void TearDown() override {
-     // Code here will be called immediately after each test (right
-     // before the destructor).
+  void TearDown() override
+  {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  static void myBlankCallBack(const std_msgs::String::ConstPtr& msg)
+  {
+    // ROS_ERROR("Running callback!");
+  }
+
+  static void myCallBack(const std_msgs::String::ConstPtr& msg)
+  {
+    // ROS_INFO("%s", msg->data);
+    // ROS_ERROR("Running callback!");
+    myGlobalCounter += 1;
   }
 
   // Class members declared here can be used by all tests in the test suite
@@ -54,32 +76,51 @@ class MyTestFixture : public ::testing::Test {
 //     ASSERT_TRUE(found);
 // }
 
-TEST_F(MyTestFixture, topicPresentAndCorrectType){
-    ros::master::V_TopicInfo master_topics;
-    ros::master::getTopics(master_topics);
+TEST_F(MyTestFixture, topicPresentAndCorrectType)
+{
+  ros::master::V_TopicInfo master_topics;
+  ros::master::getTopics(master_topics);
 
-    bool found = false;
-    for (ros::master::V_TopicInfo::iterator it = master_topics.begin() ; it != master_topics.end(); it++) {
-        ROS_INFO_STREAM(it->datatype);
-        if ( desired_topic == it->name && desired_message_type == it->datatype ){
-            found = true;
-            break;
-        }
+  bool found = false;
+  for (ros::master::V_TopicInfo::iterator it = master_topics.begin(); it != master_topics.end(); it++)
+  {
+    ROS_INFO_STREAM(it->datatype);
+    if (desired_topic == it->name && desired_message_type == it->datatype)
+    {
+      found = true;
+      break;
     }
-    ASSERT_TRUE(found);
+  }
+  ASSERT_TRUE(found);
 }
 
-// TEST_F(MyTestFixture, receiveSpam){
+TEST_F(MyTestFixture, ableToSubscribe)
+{
+  ros::Subscriber sub = n.subscribe(desired_topic, 100, myBlankCallBack);
+  ros::Duration(0.2).sleep();  // give it some time to connect to any publishers
+  ros::spinOnce();
+  ASSERT_EQ(sub.getNumPublishers(), 1);
+}
 
-// }
+TEST_F(MyTestFixture, receiveSpamAtCorrectFrequency)
+{
+  ros::Subscriber sub = n.subscribe(desired_topic, 100, myCallBack);
+  ros::Duration(0.5).sleep();  // give it some time to connect to any publishers
+  ros::spinOnce();
+  myGlobalCounter = 0;
+  ros::Duration(0.5).sleep();
+  ros::spinOnce();
+  sub.shutdown();
+  // should get around 5 messages in this time, allow 4 to 6
+  ROS_ERROR("Received %d messages in 0.5 seconds", myGlobalCounter);
+  ASSERT_LE(myGlobalCounter, 6);
+  ASSERT_GE(myGlobalCounter, 4);
+}
 
-// TEST(TestSuite, spamFrequency){
-
-// }
-
-int main(int argc, char **argv){
-    testing::InitGoogleTest(&argc, argv);
-    ros::init(argc, argv, "tester");
-    ros::NodeHandle nh;
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "tester");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
 }
